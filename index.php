b26a71d40140210f4ba1a56dd3eb8bc6c8360394
aoalv <aoalv@192.168.25.251>
2018-07-13 20:50:25 -0300

    Esse é meu segundo commit!!

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP - </title>
+    </head>
+    <body>
+        <h1></h1><hr>
+        <?php
+        require('./inc/Config.inc.php');
+
+        // código do corpo do site
+        ?>
+    </body>
+</html>
